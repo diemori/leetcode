d59665c09d68931118fd4935487425e833fda84c
inwooro <inwooro@gmail.com>
2018-02-09 23:19:16 +0900

    [mod] best practice debugging

--- 04_median_of_two_sorted_arrays.py
+++ 04_median_of_two_sorted_arrays.py
@@ -1,4 +1,45 @@
 class Solution:
+    def median(self, A, B):
+        m, n = len(A), len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+        if n == 0:
+            raise ValueError
+
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+
+            if i < m and B[j - 1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i - 1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+
+                if i == 0:
+                    max_of_left = B[j - 1]
+                elif j == 0:
+                    max_of_left = A[i - 1]
+                else:
+                    max_of_left = max(A[i - 1], B[j - 1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
+
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -57,9 +98,13 @@ class Solution:
 
 s = Solution()
 
+# num1 = [1, 2, 4, 6, 22, 24]
+# num2 = [2, 3, 5, 11, 15]
+#
+# med1 = s.findMedianSortedArrays(num1, num2)
+
 num1 = [1, 2, 4, 6, 22, 24]
 num2 = [2, 3, 5, 11, 15]
+med2 = s.median(num1, num2)
 
-med = s.findMedianSortedArrays(num1, num2)
-
-print(med)
+print(med2)
