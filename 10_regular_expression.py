3f18018a207c8a23437660fedb611d0435929808
unknown <inwooro@gmail.com>
2018-02-20 14:52:22 +0900

    [mod] comment and convention

--- 10_regular_expression.py
+++ 10_regular_expression.py
@@ -3,18 +3,32 @@ class Solution(object):
         memo = {}
 
         def dp(i, j):
-            if (i, j) not in memo:
-                if j == len(pattern):
-                    ans = i == len(text)
-                else:
-                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
-                    if j+1 < len(pattern) and pattern[j+1] == '*':
-                        ans = dp(i, j+2) or first_match and dp(i+1, j)
-                    else:
-                        ans = first_match and dp(i+1, j+1)
+            if (i, j) in memo:
+                return memo[i, j]
 
-                memo[i, j] = ans
+            if j == len(pattern):
+                ans = i == len(text)
+            else:
+                first_match = i < len(text) and pattern[j] in {text[i], '.'}
+
+                if j+1 < len(pattern) and pattern[j+1] == '*':
+                    # try both (ignoring *) and (using *)
+                    ans = dp(i, j+2) or first_match and dp(i+1, j)
+                else:
+                    # go to next char
+                    ans = first_match and dp(i+1, j+1)
+
+            memo[i, j] = ans
+
             return memo[i, j]
 
         return dp(0, 0)
 
+
+if __name__ == "__main__":
+    s = Solution()
+
+    input_pair = ("aa", "a*a")
+    result = s.isMatch(*input_pair)
+
+    print(result)
